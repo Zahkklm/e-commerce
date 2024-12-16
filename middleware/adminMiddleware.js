d5d9e5cb19a2431a960eba9e2b9b589631e371cb
Özgür Peynirci <ozgurpeynirci@gmail.com>
2024-12-16 19:22:42 +0300

    Create adminMiddleware

---
+++ middleware/adminMiddleware.js
@@ -0,0 +1,9 @@
+const adminMiddleware = (req, res, next) => {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ error: 'Access denied, admin only' });
+    }
+    next();
+  };
+  
+  module.exports = adminMiddleware;
+  
