9fff2c75562a5d0f2649419ffbbd5aaa4fd909ca
Özgür Peynirci <ozgurpeynirci@gmail.com>
2024-12-21 07:43:16 +0300

    Register productService API calls to backend

---
+++ frontend/src/services/productService.js
@@ -0,0 +1,22 @@
+import api from './api';
+
+const productService = {
+  getProducts: async (filters = {}) => {
+    const { data } = await api.get('/products', { params: filters });
+    return data;
+  },
+
+  getProductById: async (id) => {
+    const { data } = await api.get(`/products/${id}`);
+    return data;
+  },
+
+  searchProducts: async (searchTerm) => {
+    const { data } = await api.get('/products/search', { 
+      params: { q: searchTerm } 
+    });
+    return data;
+  }
+};
+
+export default productService;
